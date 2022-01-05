b674930b7e003c70e556fc89ff255cdef44dbe2a
Aditi Kalaskar <80143404+aditikalaskar0409@users.noreply.github.com>
2022-01-04 20:14:09 -0800

    Add files via upload

---
+++ StackMain.java
@@ -0,0 +1,24 @@
+package javaCollection;
+public class StackMain
+{
+
+    public static void main(String[] args) {
+
+
+        Stack stack = new Stack();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.display();
+
+        //stack.peek();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.display();
+
+    }
+
+
+}
